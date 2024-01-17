066b0c81e4434c25201eaaf21fb62ae8a546b3a7
AntoineTSIO <antoine.touzart@outlook.com>
2024-01-17 15:11:03 +0100

    exo4

--- src/fr/hymaia/exo4/scala_udf.py
+++ src/fr/hymaia/exo4/scala_udf.py
@@ -1,9 +1,23 @@
-import pyspark.sql.functions as f
 from pyspark.sql import SparkSession
+from pyspark.sql.column import Column, _to_java_column, _to_seq
+from pyspark.sql.functions import col
+
+spark = SparkSession.builder.master("local[*]").appName("scala_udf").config('spark.jars', 'src/resources/exo4/udf.jar').getOrCreate()
 
 
-spark = SparkSession.builder.appName("exo4").master("local[*]").getOrCreate()
+def addCategoryName(col):
+    # on récupère le SparkContext
+    sc = spark.sparkContext
+    # Via sc._jvm on peut accéder à des fonctions Scala
+    add_category_name_udf = sc._jvm.fr.hymaia.sparkfordev.udf.Exo4.addCategoryNameCol()
+    # On retourne un objet colonne avec l'application de notre udf Scala
+    return Column(add_category_name_udf.apply(_to_seq(sc, [col], _to_java_column)))
+
 
 def main():
-    print("Hello world!")
+    sell_df = spark.read.csv("src/resources/exo4/sell.csv", header=True, inferSchema=True)
 
+    sell_df = sell_df.withColumn("category", addCategoryName(col("category")))
+    sell_df.show()
+
+    spark.stop()
